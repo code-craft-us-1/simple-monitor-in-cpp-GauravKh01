5dd95d38b7516ca7686d3b71a5bf255b47766e68
“Khandelwal <Gaurav.Khandelwal@philips.com>
2024-10-11 06:11:23 -0700

    fixing build error

--- monitor.cpp
+++ monitor.cpp
@@ -9,9 +9,19 @@ using std::this_thread::sleep_for;
 using std::chrono::seconds;
 
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-    if (temperature > 102 || temperature < 95) { return printOutput("Temperature is critical!\n");}
-    else if (pulseRate < 60 || pulseRate > 100) {return printOutput("Pulse Rate is out of range!\n");}
-    else if (spo2 < 90) {return printOutput("Oxygen Saturation out of range!\n");}
+    if (temperature > 102 || temperature < 95) 
+    { 
+        return printOutput("Temperature is critical!\n");
+    }
+    else if (pulseRate < 60 || pulseRate > 100)
+    {
+        return printOutput("Pulse Rate is out of range!\n");
+    }
+    else if (spo2 < 90) 
+    {
+        return printOutput("Oxygen Saturation out of range!\n");
+    }
+
     return 1;
 }
 
