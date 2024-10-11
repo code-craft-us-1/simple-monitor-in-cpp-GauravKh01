13912c53ba9a05dbf9b4edcdfa70898a71ae84e8
Gaurav Khandelwal <110447594+GauravKh01@users.noreply.github.com>
2024-10-11 05:55:37 -0700

    Update monitor.cpp
    updated method to remove duplicate code

--- monitor.cpp
+++ monitor.cpp
@@ -6,33 +6,27 @@
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
+    if (temperature > 102 || temperature < 95) {
+        return printOutput("Temperature is critical!\n");
+    }
+    else if (pulseRate < 60 || pulseRate > 100) {
+        return printOutput("Pulse Rate is out of range!\n");
+    }
+    else if (spo2 < 90) {
+        return printOutput("Oxygen Saturation out of range!\n");
+    }
+    return 1;
+}
+
+int printOutput(std::string message)
+{
+    cout << message;
+
     for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
+        cout << "\r* " << flush;
+        sleep_for(seconds(1));
+        cout << "\r *" << flush;
+        sleep_for(seconds(1));
     }
     return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  }
-  return 1;
 }
