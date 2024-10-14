97809207be618c3202befa8317f4b18ed9a1b76a
Gaurav Khandelwal <110447594+GauravKh01@users.noreply.github.com>
2024-10-14 04:00:35 -0700

    Update monitor.cpp
    fixing build errors

--- monitor.cpp
+++ monitor.cpp
@@ -3,6 +3,7 @@
 #include <thread>
 #include <chrono>
 #include <iostream>
+#include <string>
 
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
@@ -13,7 +14,7 @@ constexpr float MIN_PULSE = 60;
 constexpr float MIN_SPO2 = 90;
 constexpr float MAX_SPO2 = 100;
 
-bool isParametersNormal(float value, float max, float min, const char* vitalName) {
+bool isVitalNormal(float value, float max, float min, const char* vitalName) {
     if (value < min || value > max) {
         printOutput(vitalName);
         return false;
@@ -22,14 +23,14 @@ bool isParametersNormal(float value, float max, float min, const char* vitalName
 }
 
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-    bool isTempNormal = isParametersNormal(temperature, MAX_TEMP, MIN_TEMP, "Temperature is critical!\n");
-    bool isPulseNormal = isParametersNormal(pulseRate, MAX_PULSE, MIN_PULSE, "Pulse Rate is out of range!\n");
-    bool isOxygenNormal = isParametersNormal(spo2, MAX_SPO2, MIN_SPO2, "Oxygen Saturation out of range!\n");
+    bool isTempNormal = isVitalNormal(temperature, MAX_TEMP, MIN_TEMP, "Temperature is critical!\n");
+    bool isPulseNormal = isVitalNormal(pulseRate, MAX_PULSE, MIN_PULSE, "Pulse Rate is out of range!\n");
+    bool isOxygenNormal = isVitalNormal(spo2, MAX_SPO2, MIN_SPO2, "Oxygen Saturation out of range!\n");
 
     return  isTempNormal && isPulseNormal && isOxygenNormal;
 }
 
-void printOutput(std::string message){
+void printOutput(std::string message) {
     cout << message;
     for (int i = 0; i < 6; i++) {
         cout << "\r* " << flush;
